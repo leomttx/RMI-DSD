6de3a343b629a65cf6122bb859c6dfcab6d15f15
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-07-19 23:02:24 +0000

    ✨ objeto no ar

--- python/ListaDeChamada.py
+++ python/ListaDeChamada.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+import Pyro4
 
+@Pyro4.expose
 class ListaDeChamada():
     def __init__(self, disciplina, data = None):
         self.alunos = []
--- python/cliente copy.py
+++ python/cliente copy.py
@@ -2,5 +2,7 @@ import Pyro4, sys
 
 nome = sys.argv[1].strip()
 
-greeting_maker = Pyro4.Proxy("PYRONAME:example.greeting")
-print(greeting_maker.get_fortune(nome))
+lista_de_chamada = Pyro4.Proxy("PYRONAME:lista-de-chamada")
+lista_de_chamada.adicionarAluno(nome)
+print(lista_de_chamada.verAlunos())
+print("Disciplina: ", lista_de_chamada.verDisciplina())
